4dbc2009eda6a8462eb8ef880abbea806b70261a
Jaden Abrams <jlabrams@ncsu.edu>
2024-06-18 23:23:19 -0400

    use discord.py command API, JSON parsing

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from typing import Final
 import os
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
-from responses import get_response
+from discord.ext import commands
+from responses import get_response, load_json
 
 # Load in token
 load_dotenv()
@@ -13,46 +14,44 @@ print(TOKEN)
 # Bot setup
 intents: Intents = Intents.default()
 intents.message_content = True
-client: Client = Client(intents = intents)
+client: Client = commands.Bot(command_prefix='!' ,intents = intents)
 
 # Message functionality
-
-async def send_message(message: Message, user_message: str) -> None:
-    if not user_message:
-        print("(user message was empty, intents might not be configured right)")
-        return
-    if user_message[0] != '!':
-        return
-    user_message = user_message[1:]
-
+def process_message(user_message: str) -> str:
+    additionalNames: int = -1
     try:
-        response: str = get_response(user_message)
-        await message.channel.send(response)
+        additionalNames = int(user_message)
+    except:
+        additionalNames = 0
+    
+    if additionalNames < 0:
+        return "Nice Try"
+    if additionalNames > 150:
+        return "Too many middle names"
+    try:
+        response: str = get_response(additionalNames)
+        return response
     except Exception as e:
         print(e)
-    
+        return ":("
+
+# The main command
+@client.command(name="baby")
+async def test(ctx: commands.Context, arg: str = "") -> None:
+    try:
+        await ctx.send(process_message(arg))
+    except Exception as e:
+        print("Something happened, and we can't respond")
+        print(e)
 # Handling Startup
 @client.event
 async def on_ready() -> None:
     print(f'{client.user} is now running!')
 
 
-# Handling incoming message
-
-@client.event
-async def on_message(message: Message) -> None:
-    if message.author == client.user:
-        return
-    
-    username: str = str(message.author)
-    user_message: str = message.content
-    channel: str = str(message.channel)
-
-    print(f'[{channel}] {username}: "{user_message}"')
-    await send_message(message, user_message)
-
 # Main function
 def main() -> None:
+    load_json()
     client.run(token=TOKEN)
 
 # Entry point
--- responses.py
+++ responses.py
@@ -1,4 +1,16 @@
+from json import load, dump
+from random import choice, choices
 
+namesRepo = {}
 
-def get_response(user_input: str) -> str:
-    return 'Hello!!!!'
+def get_response(num_middle_names: int) -> str:
+    first: str = choice(namesRepo["first"])
+    middle_names_list: list[str] = choices(namesRepo["middle"], k=num_middle_names)
+    middle: str = " ".join(middle_names_list)
+    last: str = choice(namesRepo["last"])
+    return first + " " + middle + " " + last
+
+def load_json() -> None:
+    with open('names.json', 'r') as f:
+        global namesRepo
+        namesRepo = load(f)
